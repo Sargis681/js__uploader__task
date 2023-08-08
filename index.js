d0a3d646345edb909a33d360c5554ee0c1a1ca6a
Sargis681 <sargis.1998@mail.ru>
2023-08-08 16:25:05 +0400

    I separated upload and onload from the uploadBatch function

--- index.js
+++ index.js
@@ -13,17 +13,19 @@ const files = [];
 let unloading = true;
 let fileToDrag = 0;
 let container = [];
+const start = 0;
+const end = 3;
 
 function retrievalMethod(newFiles) {
   files.push(...newFiles);
   displayFiles();
   if (unloading) {
     unloading = false;
-
+    console.log(allUploads);
     if (files.length - allUploads >= 3) {
-      uploadBatch(0, 3);
+      uploadBatch(start, end);
     } else {
-      uploadBatch(0, files.length - allUploads);
+      uploadBatch(start, files.length - allUploads);
     }
   }
 }
@@ -105,7 +107,6 @@ function displayFiles() {
       let fileSize = document.createElement("div");
       fileSize.className = "file-size";
       fileSize.textContent = (file.size / (1024 * 1024)).toFixed(2) + " MB";
-
       containerCol2.appendChild(containerFile);
       containerCol2.appendChild(containerProgress);
       containerCol2.appendChild(fileSize);
@@ -128,6 +129,28 @@ function typeValidation(fileType) {
   );
 }
 
+function handleProgress(e, containerFile, progressSpan) {
+  if (e.lengthComputable) {
+    const percentComplete = ((e.loaded / e.total) * 100).toFixed(2);
+    containerFile.innerHTML = percentComplete;
+    progressSpan.style.width = `${percentComplete}%`;
+  }
+}
+
+function handleOnload() {
+  allUploads++;
+  fileToDrag++;
+  if (allUploads === files.length) {
+    unloading = true;
+    fileToDrag = 0;
+    container = [];
+    return;
+  } else if (fileToDrag === end) {
+    unloading = false;
+    uploadBatch(end, end + files.length - allUploads);
+  }
+}
+
 function uploadBatch(start, end) {
   for (let i = start; i < end; i++) {
     const xhr = new XMLHttpRequest();
@@ -139,27 +162,20 @@ function uploadBatch(start, end) {
     const progress = container[i];
     const containerFile = progress.querySelector(".container__file span");
     const progressSpan = progress.querySelector(".container__progress span");
-    xhr.upload.onprogress = (e) => {
-      if (e.lengthComputable) {
-        const percentComplete = ((e.loaded / e.total) * 100).toFixed(2);
-        containerFile.innerHTML = percentComplete;
-        progressSpan.style.width = `${percentComplete}%`;
-      }
-    };
+
+    xhr.upload.onprogress = (e) =>
+      handleProgress(e, containerFile, progressSpan);
+
     xhr.onload = () => {
       if (xhr.status === 200) {
-        allUploads++;
-        fileToDrag++;
-
-        if (allUploads === files.length) {
-          unloading = true;
-          fileToDrag = 0;
-          container = [];
-          return;
-        } else if (fileToDrag === end) {
-          unloading = false;
-          uploadBatch(end, end + Math.min(3, files.length - allUploads));
-        }
+        handleOnload(
+          allUploads,
+          fileToDrag,
+          files.length,
+          container,
+          unloading,
+          end
+        );
       }
     };
 
